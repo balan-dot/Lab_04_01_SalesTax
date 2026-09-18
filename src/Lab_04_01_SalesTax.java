af72b4475a333a033fa96a97db2887f5c28c93fa
ryanb <balan@mail.uc.edu>
2026-09-18 10:18:57 -0400

    Initial commit

---
+++ src/Lab_04_01_SalesTax.java
@@ -0,0 +1,14 @@
+public class Lab_04_01_SalesTax {
+static void main() {
+
+    double price = 12.50;
+    IO.println("The price of this item is: $" + price);
+
+    double salesTax = price * 0.05;
+    IO.println(salesTax);
+
+
+
+
+    }
+}
